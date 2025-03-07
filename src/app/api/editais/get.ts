f68a1f164f81cafd1ca81cf9475112a717d20cc4
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2025-03-07 13:45:15 -0300

    fix: routes for get and post now working correctly

---
+++ src/app/api/editais/get.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+import RepositoryFactory from "@/domain/factories/RepositoryFactory";
+
+export async function GET(req: NextRequest) {
+  try {
+    const editais = await RepositoryFactory.getEditalRepository().findAll();
+
+    return NextResponse.json(editais, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "Erro ao buscar editais: " + error.message },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/editais/post.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+import createEditalService from "@/domain/services/createEditalService";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json(); // Parse the request body
+    const { title, icon, description, funding_min, funding_max, sponsor, sdgs, causes, skills } = body;
+
+    const edital = createEditalService.create({ title, icon, description, funding_min, funding_max, sponsor, sdgs, causes, skills });
+
+    return NextResponse.json({ message: "Edital criado", edital }, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}

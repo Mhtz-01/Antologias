97cff6ad6d1354353a0b679b7b5d59c6bab00b96
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2025-03-19 11:14:39 -0300

    fix global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
-import Home from "./page";
+import Navbar from '@/components/navbar';
 import './globals.css';
 
-export default function RootLayout() {
-    return (
-      <html lang="pt-br">
-        <body>
-            <Home/>
-        </body>
-      </html>
-    );
-  }
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <title>Antologias</title>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body>
+        <Navbar/>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}

9d5b68c6689cfcc7a4d1ad3eb70c9a8c6e2a86cf
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2025-03-20 11:04:40 -0300

    style: new login modal

---
+++ src/components/filter/editaisfilter.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import FilterButton from "./filterbutton";
+import FilterField from "./filterfield";
+
+interface SelectedFilters {
+    area: string[];
+    ods: string[];
+    region: string[];   
+    skills: string[];
+}
+
+export default function FilterSection() {
+    const [selectedFilters, setSelectedFilters] = useState<SelectedFilters>({
+        area: [],
+        ods: [],
+        region: [],
+        skills: []
+    });
+
+    const area = ["Sustentabilidade", "Artes e Cultura", "Educação", "Tecnologia", "Diversidade"];
+    const ods = ["ODS1", "ODS2", "ODS3", "ODS4", "ODS5"]
+    const region = ["Municipal", "Estadual", "Federal", "Internacional"];
+    const skills = ["Artes", "Dança", "Artesanato", "Tecnologia", "Saúde", "Cozinha"]
+
+    // Enviar filtros ao backend
+    const applyFilters = async () => {
+        console.log("Filtros selecionados:", selectedFilters);
+        try {
+            const response = await fetch("/api/filter", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(selectedFilters)
+            });
+
+            if (!response.ok) throw new Error("Erro ao buscar os dados.");
+
+            const data = await response.json();
+            console.log("Resultados:", data);
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+        }
+    };
+
+    return (
+        <div className="flex flex-col place-items-center text-slate-900 my-3 gap-2 overflow-y-auto">
+            <span>Filtre sua busca por aqui</span>
+            <FilterField label="Causas de interesse" options={area} selectedFilters={selectedFilters.area} setSelectedFilters={(filters) => setSelectedFilters(prev => ({ ...prev, area: filters }))} />
+            <FilterField label="ODS" options={ods} selectedFilters={selectedFilters.ods} setSelectedFilters={(filters) => setSelectedFilters(prev => ({ ...prev, ods: filters }))} />
+            <FilterField label="Habilidades requisitadas" options={skills} selectedFilters={selectedFilters.skills} setSelectedFilters={(filters) => setSelectedFilters(prev => ({ ...prev, skills: filters }))} />
+            <FilterField label="Região" options={region} selectedFilters={selectedFilters.region} setSelectedFilters={(filters) => setSelectedFilters(prev => ({ ...prev, region: filters }))} />
+
+            <FilterButton onClick={applyFilters} />
+        </div>
+    );
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer(){
+    return (
+        <>
+            {/* Rodapé */}
+            <footer className="w-full bg-pcr text-white flex flex-col md:flex-row items-center justify-center gap-4 p-4 text-sm text-center md:text-left">
+                <a href="https://portal.cin.ufpe.br/" target="_blank" rel="noopener noreferrer">
+                    <img
+                        className="w-20 md:w-28 object-contain"
+                        src="https://i.postimg.cc/W4d6s9r6/HMB.png"
+                        alt="CIn UFPE"
+                    />
+                </a>
+                <span>
+                    Esse site é uma iniciativa do projeto CIn-Bora Impactar em parceria com a Prefeitura de Recife.
+                </span>
+            </footer>
+        </>
+    )
+}
